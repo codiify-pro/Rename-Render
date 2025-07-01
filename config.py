64d8cee907efc473d4d1bf9f93e8d142cbfc74e0
Codiify <aaryansah954@gmail.com>
2025-07-01 13:54:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz")
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://premkumarsha02:pEydsi4IFcJkxaws@cluster0.b4vrd9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
